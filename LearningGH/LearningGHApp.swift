f45ef684ab8a19c1e3b55dbf8a9c337cf1587c7a
Xaivery <xaiveryr@outlook.com>
2022-07-27 21:35:29 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Andrea Delgado on 2022-07-27.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
